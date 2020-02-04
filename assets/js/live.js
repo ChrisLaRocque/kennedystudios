b2c1cc66b4dcaf9a4b092079cf1445ed7205599f
Chris LaRocque <larocque.christopher@gmail.com>
2020-02-04 17:02:18 -0500

    Starts featured video

--- assets/js/live.js
+++ assets/js/live.js
@@ -10,7 +10,7 @@ for (var i = 0; i < session.length; i++){
 	<div class="col-xl-6 col-12 vid-img ` +  id +`-cover anim d-flex justify-content-center" data-delay=".75s" title="` + title + `">
 		<i class="fas fa-play-circle align-self-center"></i>
 		<h2 class="align-self-end">` + name + `</h2>
-		<div class="embed-responsive embed-responsive-16by9">
+		<div class="embed-responsive embed-responsive-16by9 embed-panel">
 			<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/` + tube + `?rel=0"
 				allowfullscreen></iframe>
 		</div>
