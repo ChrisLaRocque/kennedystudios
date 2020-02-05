58fb901f2df6b62b8bd34391a1bfa8124a5d58dd
Chris <larocque.christopher@gmail.com>
2020-02-04 21:30:02 -0500

    Added text sections + cleaned up code

--- assets/js/live.js
+++ assets/js/live.js
@@ -1,22 +1,34 @@
-const session = document.querySelectorAll('session');
+const session = document.querySelectorAll("session");
 
-for (var i = 0; i < session.length; i++){
-	var id = session[i].id;
-	var title = session[i].title;
-	var tube = session[i].dataset.tube;
-	var name = session[i].dataset.name;
+for (var i = 0; i < session.length; i++) {
+  var id = session[i].id;
+  var title = session[i].title;
+  var tube = session[i].dataset.tube;
+  var name = session[i].dataset.name;
+  var delay = session[i].dataset.delay;
 
-	const template = `
-	<div class="col-xl-6 col-12 vid-img ` +  id +`-cover anim d-flex justify-content-center" data-delay=".75s" title="` + title + `">
+  const template =
+    `
+	<div class="col-xl-6 col-12 vid-img ` +
+    id +
+    `-cover anim anim-down d-flex justify-content-center" data-delay="` +
+    delay +
+    `" title="` +
+    title +
+    `">
 		<i class="fas fa-play-circle align-self-center"></i>
-		<h2 class="align-self-end">` + name + `</h2>
+		<h2 class="align-self-end">` +
+    name +
+    `</h2>
 		<div class="embed-responsive embed-responsive-16by9 embed-panel">
-			<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/` + tube + `?rel=0"
+			<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/` +
+    tube +
+    `?rel=0"
 				allowfullscreen></iframe>
 		</div>
 	</div>
 	`;
-	
-	// replace el with newEL
-	$(".session").append(template);
-};
+
+  // replace el with newEL
+  $(".session").append(template);
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,59 @@
 // HTML Loader
-var ids = $(".load").map(function () {
+var ids = $(".load").map(function() {
   return $(this).attr("id");
 });
 
 for (var i = 0; i < ids.length; i++) {
   const id = ids[i];
-  $.get(id + ".html", function (data) {
+  $.get(id + ".html", function(data) {
     $("#" + id).replaceWith(data);
   });
 }
+
+// Anim Down
+const animDown = document.querySelectorAll(".anim-down");
+
+observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.style.animation = `anim1 2s ${entry.target.dataset.delay} forwards ease-out`;
+    } else {
+      entry.target.style.animation = `none`;
+    }
+  });
+});
+animDown.forEach(image => {
+  observer.observe(image);
+});
+
+// Anim Right
+const animRight = document.querySelectorAll(".anim-right");
+
+observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.style.animation = `anim2 3s ${entry.target.dataset.delay} forwards ease-out`;
+    } else {
+      entry.target.style.animation = `none`;
+    }
+  });
+});
+animRight.forEach(image => {
+  observer.observe(image);
+});
+
+// Anim Left
+const animLeft = document.querySelectorAll(".anim-left");
+
+observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.style.animation = `anim3 3s ${entry.target.dataset.delay} forwards ease-out`;
+    } else {
+      entry.target.style.animation = `none`;
+    }
+  });
+});
+animLeft.forEach(image => {
+  observer.observe(image);
+});
